c79d6c33bfab23ddee5aa7a6bf840ebcf0138a89
Nguyen On <ohn24042000@gmail.com>
2022-01-19 15:40:10 +0700

    Add descriptor for widget types

--- server/models/widgetType.js
+++ server/models/widgetType.js
@@ -9,6 +9,7 @@ const defaultWidgetTypes = [
     bundleAlias: constant.DEFAULT_WIDGETS_BUNDLE.CHARTS,
     description:
       "Displays changes to timeseries data over time. For example, daily water consumption for last month.",
+    descriptor: '{"sizeX":4,"sizeY":3}',
   },
   {
     name: "Line Chart",
@@ -16,12 +17,15 @@ const defaultWidgetTypes = [
     bundleAlias: constant.DEFAULT_WIDGETS_BUNDLE.CHARTS,
     description:
       "Displays changes to timeseries data over time. For example, temperature or humidity readings.",
+    descriptor: '{"sizeX":4,"sizeY":3}',
   },
   {
     name: "Pie Chart",
     alias: constant.DEFAULT_WIDGET_TYPE.PIE_CHART,
     bundleAlias: constant.DEFAULT_WIDGETS_BUNDLE.CHARTS,
-    description: "Displays latest values of the attributes or timeseries data for multiple entities in a pie chart. Supports numeric values only."
+    description:
+      "Displays latest values of the attributes or timeseries data for multiple entities in a pie chart. Supports numeric values only.",
+    descriptor: '{"sizeX":2,"sizeY":2}',
   },
 
   // ANALOGUE GAUGES
@@ -29,13 +33,17 @@ const defaultWidgetTypes = [
     name: "Linear Gauge",
     alias: constant.DEFAULT_WIDGET_TYPE.LINEAR_GAUGE,
     bundleAlias: constant.DEFAULT_WIDGETS_BUNDLE.ANALOGUE_GAUGES,
-    description: "Preconfigured widget to display temperature. Allows to configure temperature range, gradient colors and other settings."
+    description:
+      "Preconfigured widget to display temperature. Allows to configure temperature range, gradient colors and other settings.",
+    descriptor: '{"sizeX":6,"sizeY":2}',
   },
   {
     name: "Radial Gauge",
     alias: constant.DEFAULT_WIDGET_TYPE.RADIAL_GAUGE,
     bundleAlias: constant.DEFAULT_WIDGETS_BUNDLE.ANALOGUE_GAUGES,
-    description: "Preconfigured gauge to display any value reading. Allows to configure value range, gradient colors and other settings."
+    description:
+      "Preconfigured gauge to display any value reading. Allows to configure value range, gradient colors and other settings.",
+    descriptor: '{"sizeX":3,"sizeY":3}',
   },
 ];
 
@@ -107,9 +115,9 @@ module.exports = (sequelize, Sequelize) => {
     defaultWidgetTypes.forEach((widget) => {
       WidgetType.findOrCreate({
         where: {
-          ...widget
+          ...widget,
         },
-        logging: false
+        logging: false,
       });
     });
   };
